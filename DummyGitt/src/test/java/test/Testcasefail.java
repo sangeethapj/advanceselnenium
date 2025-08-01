f7fc369071078d29a77f1164c5dc636c63f3007c
57 <57@DESKTOP-EBTHUUN.lan>
2025-08-01 17:48:14 +0530

    commiting

---
+++ DummyGitt/src/test/java/test/Testcasefail.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class Testcasefail {
+@Test
+public void testfail() {
+	String status="false";
+	Assert.assertEquals(status, true,"Failed : ");
+	Reporter.log("Succesfully created campaign    ",true);
+	
+	Reporter.log("Checkingggggggg",true);
+	}
+}
